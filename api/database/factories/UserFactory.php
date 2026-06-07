806be8479542fb8c0ab1e82cc2c7590c9466e4fb
babalili <157334606+balilirichel@users.noreply.github.com>
2026-06-07 17:07:24 +0800

    Use instance faker in UserFactory for name and email

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -28,8 +28,8 @@ class UserFactory extends Factory
         $faker = \Faker\Factory::create();
         
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
